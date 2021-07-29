9bb665a2db78556baeea95591de3edf1bbe732ba
Jon Tokugawa <79482239+Joldon@users.noreply.github.com>
2021-07-29 13:23:46 +0200

    setting up the project

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Img, Text, Body} from 'react-bootstrap/Card'
+
+const Header = ({Card, Img, Text, Body}) => {
+    return (
+        <header>
+          {/* <Card>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+            <Card.Text>
+                Some quick example text to build on the card title and make up the bulk
+                of the card's content.
+            </Card.Text>
+            </Card.Body>
+          </Card>
+          <br />
+          <Card>
+            <Card.Body>
+            <Card.Text>
+                Some quick example text to build on the card title and make up the bulk
+                of the card's content.
+            </Card.Text>
+            </Card.Body>
+            <Card.Img variant="bottom" src="holder.js/100px180" />
+          </Card> */}
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/PopularBikes.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import bikesData from './bikes.json'
+
+// const Bikes = './bikes.json'
+// console.log(Bikes)
+
+// use reduce groupBy function to determine the most popular models
+// filter three items most frequently occurring in the array. 
+// filter function should filter out only three most popular items
+// test functions
+// combine (reduce?) various similar spellings of some bike models
+
+const PopularBikes = (objectArray, property) => {
+  return objectArray.reduce
+}
+
+export default PopularBikes
